5bfa8ac269278fb538068e5a51aa74035fc6d584
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-07-26 23:59:26 +0530

    Update App.js

--- UIETexam/client/src/App.js
+++ UIETexam/client/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import Home from './components/controller1/options/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Examiners from "./components/controller1/options/Examiners";
+import Papers from "./components/controller1/options/Papers";
+import Payments from "./components/controller1/options/Payments";
+import Logout from "./components/controller1/options/Logout";
+const router = createBrowserRouter([
+  {
+    path: "/controller1/Home",
+    element: <Home/>,
+  },
+  {
+    path: "/controller1/Examiners",
+    element: <Examiners/>,
+  },
+  {
+    path: "/controller1/Papers",
+    element: <Papers/>,
+  },
+  {
+    path: "/controller1/Payments",
+    element: <Payments/>,
+  },
+  {
+    path: "/controller1/Logout",
+    element: <Logout/>,
+  },
+]);
 
-const App = () => {
+function App() {
   return (
     <>
-      <h1>hello</h1>
+       <RouterProvider router={router} />
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
