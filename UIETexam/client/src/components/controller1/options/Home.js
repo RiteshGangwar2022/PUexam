dc959fd403050709c63496ab162e363033dff06e
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-07-26 23:54:11 +0530

    Create Home.js

---
+++ UIETexam/client/src/components/controller1/options/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Controller1 from '../Controller1';
+const Home = () => {
+  return (
+    
+    <Controller1>
+    <div className=" w-full bg-red-500 mb-2 text-white uppercase  font-bold px-3 py-1 ">DashBoard</div>
+        <div>
+            options will be shown here
+        </div>
+    </Controller1>
+   
+    
+  )
+}
+
+export default Home
