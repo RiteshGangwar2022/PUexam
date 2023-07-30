92ca0bbfe85bc875ba96c4e414235917e3f9560c
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-07-30 12:54:29 +0530

    home and examiner

--- UIETexam/client/src/components/controller1/options/Home.js
+++ UIETexam/client/src/components/controller1/options/Home.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import Controller1 from '../Controller1';
+import React from "react";
+import Controller1 from "../Controller1";
 const Home = () => {
   return (
-    
     <Controller1>
-    <div className=" w-full bg-red-500 mb-2 text-white uppercase  font-bold px-3 py-1 ">DashBoard</div>
-        <div>
-            options will be shown here
+      <div className=" grid  grid-cols-1   h-[calc(100vh-7rem)] md:grid-cols-3 gap-6  ">
+        <div className=" bg-white  shadow-md  rounded-xl">
+          <h1 className=" text-3xl font-bold p-2">Examiners</h1>
         </div>
-    </Controller1>
-   
-    
-  )
-}
+        <div className=" bg-white   shadow-md  rounded-xl">
+          <h1 className=" text-3xl font-bold p-2">Payments</h1>
+        </div>
 
-export default Home
+        <div className=" bg-white row-span-2   shadow-md  rounded-xl">
+          <h1 className=" text-3xl font-bold p-2">Activities</h1>
+        </div>
+
+        <div className=" bg-white   shadow-md  rounded-xl">
+          <h1 className=" text-3xl font-bold p-2">Sessions</h1>
+        </div>
+        <div className=" bg-white   shadow-md  rounded-xl">
+          <h1 className=" text-3xl font-bold p-2">Subjects</h1>
+        </div>
+      </div>
+    </Controller1>
+  );
+};
+
+export default Home;
