8c39c0dad6721add21ec5fd2ba7d96e6b285d03c
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-08-01 00:30:26 +0530

    Create Examiner.js

---
+++ UIETexam/client/src/components/Examiner/Examiner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Topbar from "./Topbar";
+import Sidebar from "./Sidebar";
+
+const Examiner = ({ children }) => {
+  return (
+    <>
+      <div className="flex-1 flex flex-col ">
+        <Topbar />
+        <div className={`flex font-["IBM_Plex_Sans"] h-screen "bg-[#F8F9FF]"`}>
+          <Sidebar />
+          <div className="flex-1 overflow-y-scroll p-4 bg-gray-200">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Examiner;
