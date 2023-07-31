688027fab5095df44f449594c87455833f2bfdc2
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-08-01 00:33:47 +0530

    Add files via upload

---
+++ UIETexam/client/src/components/Examiner/options/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Examiner from '../Examiner'
+const Logout = () => {
+  return (
+    <Examiner>
+      This is Logout page
+    </Examiner>
+  )
+}
+
+export default Logout
---
+++ UIETexam/client/src/components/Examiner/options/Payments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Examiner from '../Examiner'
+const Payments = () => {
+  return (
+    <Examiner>
+      This is Payments Page
+    </Examiner>
+  )
+}
+
+export default Payments
