88ca07dccc34d83df7df091c82714c06b5118df6
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-08-01 13:49:52 +0530

    Update Login.js

--- UIETexam/client/src/components/auth/Login.js
+++ UIETexam/client/src/components/auth/Login.js
@@ -5,8 +5,6 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [showOTPVerification, setShowOTPVerification] = useState(false);
-    const [otp, setOTP] = useState('');
     const navigate = useNavigate();
     async function submit(e){
 
@@ -19,7 +17,7 @@ const Login = () => {
               })
               .then(res=>{
                 if(res.data=='exist'){
-                    setShowOTPVerification(true);
+                  navigate('/otpPage');
                 }
                 else if(res.data=="notexist") {
                   alert("User Not Registered");
@@ -27,6 +25,7 @@ const Login = () => {
               })
               .catch(e=>{
                 alert("Wrong");
+                navigate('/otpPage');
                 console.log(e);
               })
       
@@ -36,28 +35,7 @@ const Login = () => {
             }
       
        }
-       const handleVerifyOTP = async () => {
-        try {
-          // Make an API call to your backend server or directly to the OTP service API
-          // with the OTP entered by the user and the user's email/phone for verification
-          const response = await axios.post('/api/verify-otp', {
-            email: email, // Replace 'userEmail' with the user's email
-            otp: otp, // Replace 'otp' with the OTP entered by the user
-          });
-      
-          if (response.data === 'success') {
-            // OTP verification successful, handle success accordingly
-            setShowOTPVerification(false);// Close the OTP verification window
-            navigate('/controller1/Home');
-          } else {
-            // OTP verification failed, handle failure accordingly
-            alert('Invalid OTP. Please try again.');
-          }
-        } catch (error) {
-          console.error('Error verifying OTP:', error);
-          alert('Error verifying OTP. Please try again later.');  
-        }
-      };
+     
       
       
 
@@ -101,26 +79,6 @@ const Login = () => {
           </button>
         </form>
       </div>
-      {showOTPVerification && ( 
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-opacity-80 bg-gray-600">
-      <div className="bg-gray-200 p-6 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-4">OTP Verification</h2>
-        <input
-          type="text"
-          value={otp}
-          onChange={(e) => setOTP(e.target.value)}
-          className="w-full border-gray-300 rounded-md shadow-md px-3 py-2 mb-2"
-          placeholder="Enter OTP"
-        />
-        <button
-          onClick={handleVerifyOTP}
-          className="bg-indigo-500 text-white rounded-md py-2 px-4 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Verify OTP
-        </button>
-      </div>
-    </div>
-      )}
     </div>
   );
 };
