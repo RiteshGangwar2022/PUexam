dfa6d82c3f70566c9812298f099a669555d20fac
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-08-21 18:44:01 +0530

    Update examiner.js

--- UIETexam/Backend/controller/examiner.js
+++ UIETexam/Backend/controller/examiner.js
@@ -130,7 +130,7 @@ const GetAssignments = async (req, res) => {
 const SingleAssignment=async(req,res)=>{
 
   try {
-    //getting id of the particular product from database on clicking to the image of that item
+ 
     const _id = req.params.id;
     console.log(_id)
     const assigment = await Exam.findById({_id})
