cbbc4da9cdd0a3ca394504c707186b15c899086b
omsingh4321 <om43singh21@gmail.com>
2023-09-04 09:14:49 +0530

    Done some changes

--- UIETexam/client/src/components/Examiner/options/Assignments.js
+++ UIETexam/client/src/components/Examiner/options/Assignments.js
@@ -9,7 +9,6 @@ const Assignments = () => {
   const [ass, setAss] = useState([]);
   const [loading, setLoading] = useState(true);
   
-
   async function getAssignments() {
   const id=globalResponseData?._id;
   if(!globalResponseData) return;
@@ -60,7 +59,7 @@ const Assignments = () => {
           <div className="px-6 py-3 text-2xl items-center flex justify-between my-3 rounded-xl bg-white">
             <p className="  font-bold">{index}</p>
             <h1 className="  ">{assignment?.Subject?.Name}</h1>
-            <h1>{assignment?._id}</h1>
+            
             <h1 className="  ">{assignment?.Subject?.SubjectCode}</h1>
             <h1 className="  ">
             
