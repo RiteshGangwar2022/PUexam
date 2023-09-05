5d47fd41c3e925a9a0b8173c8d7f7e38c79c3b2e
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-09-05 09:52:52 +0530

    ok

--- UIETexam/client/src/components/assigne/Subjects.js
+++ UIETexam/client/src/components/assigne/Subjects.js
@@ -81,19 +81,19 @@ axios
 
     return (
         <Assigne>
-            <div className="bg-white rounded mt-2 px-4 py-2 flex flex-col">
-                <div className="text-4xl font-bold items-center justify-center">{Subject}</div>
-                <div className="flex gap-14 justify-center items-center">
+                <div className="text-center text-3xl mb-3 font-bold">{Subject} </div>
+            <div className="bg-white rounded mt-2 p-3 flex flex-col">
+                {/* <div className="flex gap-14 justify-center items-center">
                     <div className='font-bold text-3xl text-black flex flex-col justify-center items-center pb-12'>
                         Assign to
                     </div>
-                    <div className="w-fit bg-white rounded-lg shadow border-2 border-gray-500">
-                        <div className="max-h-36 overflow-y-auto max-w-lg w-80">
+                    <div className="w-fit bg-white rounded-lg shadow ">
+                        <div className="max-h-36 p-3 overflow-y-auto max-w-lg w-80">
                             {examObj && examObj.Examiners.map((items, i) => (
-                                <div key={items.id} className="flex items-start border-2 border-gray-500 gap-5">
-                                    <div>{i + 1}.</div>
+                                <div key={items.id} className="flex  items-center   gap-5">
+                                    <div className=" text-xl">{i + 1}.</div>
                                     <div className="flex-grow py-[0.2669rem] rounded-md">
-                                        <p className="text-gray-800">{items.name}</p>
+                                        <p className=" text-xl text-gray-800">{items.name}</p>
                                     </div>
                                     <div className='ml-10 mr-4'>
                                         <input
@@ -116,7 +116,36 @@ axios
                             Assign Exmainers
                         </button>
                     </div>
-                </div>
+                </div> */}
+
+                <h1 className=" text-xl">Choose examiners to assign :- </h1>
+                <div className="w-full max-w-4xl  my-3  bg-white rounded-sm  ">
+                        <div className=" p-3 border-[1px] overflow-y-auto w-full max-w-lg ">
+                            {examObj && examObj.Examiners.map((items, i) => (
+                                <div key={items.id} className="flex  my-2  bg-neutral-100 px-2  items-center   gap-5">
+                                    <div className=" text-xl">{i + 1}.</div>
+                                    <div className="flex-grow py-[0.2669rem] rounded-md">
+                                        <p className=" text-xl  ">{items.name}</p>
+                                    </div>
+                                    <div className='ml-10 mr-4'>
+                                        <input
+                                            type="checkbox"
+                                            // onChange={() => handleCheckboxChange(items.id)}
+                                            onChange={() => handleCheckboxChange(items._id)}
+                                            className="  scale-150"
+                                         
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <button className="uppercase mt-3 bg-sky-500 text-white rounded-full px-4 py-2 font-bold 
+                        "
+                        onClick={sendExaminersList}
+                        >
+                            Assign Exmainers
+                        </button>
+                    </div>
             </div>
         </Assigne>
     );
--- UIETexam/client/src/components/assigne/options/Assign.js
+++ UIETexam/client/src/components/assigne/options/Assign.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Assigne from "../Assigne";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Assign = () => {
   const [subjects, setSubjects] = useState([]);
@@ -44,20 +44,32 @@ const Assign = () => {
       <Assigne>
         <h1 className="text-center text-3xl mb-3 font-bold">Subjects List</h1>
 
-        <div className="  grid  grid-cols-3 gap-3">
+        <div className="  grid  grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-3">
           {subjects.map((sub, index) => (
-            
-            <div onClick={() => handleRedirect(  `/Assigne/Subjects/${sub.Name}`, { id: sub._id,
-              Subject: sub.Name,
-               })} key={index} className=" cursor-pointer bg-white rounded-md p-3">
+            <div
+              key={index}
+              className="  relative  aspect-video  bg-white  rounded-md p-3"
+            >
               <div className=" flex gap-2">
-                <h1 className=" text-xl"> Subject </h1>
-                <h1 className=" text-xl font-semibold"> {sub.Name} </h1>
+                <h1 className=" text-2xl"> Subject </h1>
+                <h1 className=" text-2xl font-semibold"> {sub.Name} </h1>
               </div>
               <div className=" flex gap-2">
-                <h1 className="text-xl"> Subject Code </h1>
-                <h1 className=" text-xl font-semibold"> {sub.SubjectCode} </h1>
+                <h1 className="text-base"> Subject Code </h1>
+                <h1 className=" text-base font-semibold"> {sub.SubjectCode} </h1>
               </div>
+
+              <button
+                onClick={() =>
+                  handleRedirect(`/Assigne/Subjects/${sub.Name}`, {
+                    id: sub._id,
+                    Subject: sub.Name,
+                  })
+                }
+                className=" absolute  bottom-3 mx-auto bg-sky-400 px-5 py-2   rounded-full text-white font-bold text-xl "
+              >
+                Setup
+              </button>
             </div>
           ))}
         </div>
