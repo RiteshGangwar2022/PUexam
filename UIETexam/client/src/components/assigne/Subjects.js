6c68b46b77214ad97eed5bb987ec69c03d47325e
omsingh4321 <om43singh21@gmail.com>
2023-09-08 20:11:07 +0530

    Added Form

--- UIETexam/client/src/components/assigne/Subjects.js
+++ UIETexam/client/src/components/assigne/Subjects.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Assigne from "./Assigne";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import Loader from "../loader";
 import { useAuth } from "../../Context/AuthContext";
 import { Toast, toast } from "react-hot-toast";
+
 const Subjects = () => {
   const { globalResponseData, setGlobalResponseData } = useAuth();
   const location = useLocation();
   const [examObj, setExamObj] = useState();
-  const [selectedExaminers, setSelectedExaminers] = useState([]);
   const { id, Subject } = location.state;
   const [loading, setLoading] = useState(true);
-  const [option, setOption] = useState();
-  const [session, setSession] = useState();
 
-  useEffect(() => {
-    fetchExaminers();
-  }, []);
+  const optionRef = useRef(null);
+  const sessionRef = useRef(null);
+
+  const selectedExaminersRef = useRef([]);
 
   const fetchExaminers = async () => {
     try {
@@ -38,35 +37,53 @@ const Subjects = () => {
   };
 
   useEffect(() => {
-    console.log("Exmainers Slected ");
-    console.log(selectedExaminers);
-  }, [selectedExaminers]);
+    fetchExaminers();
+  }, []);
 
   const handleCheckboxChange = (examinerId) => {
-    setSelectedExaminers((prevItems) => {
-      if (prevItems.includes(examinerId)) {
-        return prevItems.filter((item) => item !== examinerId);
-      } else {
-        return [...prevItems, examinerId];
-      }
-    });
+    if (selectedExaminersRef.current.includes(examinerId)) {
+      selectedExaminersRef.current = selectedExaminersRef.current.filter(
+        (item) => item !== examinerId
+      );
+    } else {
+      selectedExaminersRef.current.push(examinerId);
+    }
   };
 
   const sendExaminersList = () => {
     const loadingToast = toast.loading("creating assignment...");
 
-    if (selectedExaminers.length === 0) {
-      toast.error("select atleast one examiner", { id: loadingToast });
+    if (selectedExaminersRef.current.length === 0) {
+      toast.error("Select at least one examiner", { id: loadingToast });
       return;
     }
+    
+    // Get the values from the refs
+    const optionValue = optionRef.current.value;
+    const sessionValue = sessionRef.current.value;
+  
+    if (!optionValue) {
+      toast.error("Select an option", { id: loadingToast });
+      return;
+    }
+    
+    if (!sessionValue) {
+      toast.error("Select a session", { id: loadingToast });
+    }
+    console.log(sessionValue);
+    console.log(optionValue);
+    console.log(selectedExaminersRef);
+    return;
     axios
-      .post("http://localhost:5000/api/r3/addassignment", {
+      .post("", {
         DOE: new Date(),
         ExamCode: "1234",
         Branch: "Cse",
         SemesterNo: 1,
-        Examiners: selectedExaminers,
+        Examiners: selectedExaminersRef.current,
         Subject: id,
+        option: optionValue,
+        session: sessionValue,
       })
       .then((response) => {
         toast.success("success", { id: loadingToast });
@@ -90,40 +107,18 @@ const Subjects = () => {
     <Assigne>
       <div className="text-center text-3xl mb-3 font-bold">{Subject} </div>
       <div className="bg-white rounded mt-2 p-3 flex flex-col">
-        <h1 className=" text-xl">Choose examiners to assign :- </h1>
-        <div className="w-full max-w-4xl   my-3  bg-white rounded-sm  ">
-          <div className=" p-3 border-[1px]  overflow-y-auto w-full max-w-lg ">
-            {examObj &&
-              examObj.Examiners.map((items, i) => (
-                <div
-                  key={items.id}
-                  className="flex  my-2  bg-neutral-100 px-2  items-center   gap-5"
-                >
-                  <div className=" text-xl">{i + 1}.</div>
-                  <div className="flex-grow py-[0.2669rem] rounded-md">
-                    <p className=" text-xl  ">{items.name}</p>
-                  </div>
-                  <div className="ml-10 mr-4">
-                    <input
-                      type="checkbox"
-                     
-                      onChange={() => handleCheckboxChange(items._id)}
-                      className="  scale-150"
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-
-          <h1 className="text-xl my-3">Choose option and session :- </h1>
-
-          <div className=" p-3 border-[1px] flex flex-col gap-2  overflow-y-auto w-full max-w-lg ">
+        <div className="p-3 border-[1px] flex flex-col gap-2  overflow-y-auto w-full max-w-lg ">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              sendExaminersList();
+            }}
+          >
             <select
               id="option"
               name="option"
               className="w-full  p-2 border rounded-md shadow-sm focus:ring focus:white bg-white"
-              //   value={option}
-              //   onChange={(e) => setOption(e.target.value)}
+              ref={optionRef}
             >
               <option value="" selected disabled hidden>
                 Choose option
@@ -136,6 +131,7 @@ const Subjects = () => {
               id="session"
               name="session"
               className="w-full  p-2 border rounded-md shadow-sm focus:ring focus:white bg-white"
+              ref={sessionRef}
             >
               <option value="" selected disabled hidden>
                 Choose session
@@ -143,15 +139,37 @@ const Subjects = () => {
               <option value="july">July</option>
               <option value="january">January</option>
             </select>
-          </div>
-
-          <button
-            className="uppercase mt-3 bg-sky-500 text-white rounded-full px-4 py-2 font-bold 
-                        "
-            onClick={sendExaminersList}
-          >
-            Assign Exmainers
-          </button>
+            {examObj && (
+              <div>
+                <h1 className="text-xl my-3">Choose examiners to assign :- </h1>
+                {examObj.Examiners.map((items, i) => (
+                  <div
+                    key={items.id}
+                    className="flex  my-2  bg-neutral-100 px-2  items-center   gap-5"
+                  >
+                    <div className=" text-xl">{i + 1}.</div>
+                    <div className="flex-grow py-[0.2669rem] rounded-md">
+                      <p className=" text-xl  ">{items.name}</p>
+                    </div>
+                    <div className="ml-10 mr-4">
+                      <input
+                        type="checkbox"
+                        className="  scale-150"
+                       
+                        onClick={() => handleCheckboxChange(items._id)}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+            <button
+              className="uppercase mt-3 bg-sky-500 text-white rounded-full px-4 py-2 font-bold"
+              type="submit"
+            >
+              Assign Examiners
+            </button>
+          </form>
         </div>
       </div>
     </Assigne>
